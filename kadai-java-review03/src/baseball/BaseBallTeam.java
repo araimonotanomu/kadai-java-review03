7a0d0a0dd1c15244863e09d1086006b98ed35272
araimonotanomu <sevenscoad7@gmail.com>
2024-06-02 22:20:26 +0900

    再提出

--- kadai-java-review03/src/baseball/BaseBallTeam.java
+++ kadai-java-review03/src/baseball/BaseBallTeam.java
@@ -7,21 +7,21 @@ public class BaseBallTeam {
     private int lose;
     private int draw;
     
-    // 宣言
-    private double rate; 
-    
-    public double getRate() {
+    double rate = getRate(win, lose);
+    public double getRate(int win, int lose) {
         return rate = (double)win / (win + lose);
     }
     
-    public void report() {
-        System.out.println(name + "の2022年の成績は" + win + "勝" + lose + "敗" + draw + "分、勝率は" + rate + "です。");
-    }
-    
-    public void setBaseBallTeam(String name, int win, int lose, int draw) {   
+    // コンストラクタ
+    public BaseBallTeam(String name, int win, int lose, int draw) {  
         this.name = name;
         this.win = win;
         this.lose = lose;
         this.draw = draw;
+    }
+    
+    public void report() {
+        getRate(win, lose);
+        System.out.println(name + "の2022年の成績は" + win + "勝" + lose + "敗" + draw + "分、勝率は" + rate + "です。");
     }
 }
--- kadai-java-review03/src/baseball/Review03.java
+++ kadai-java-review03/src/baseball/Review03.java
@@ -3,34 +3,22 @@ package baseball;
 public class Review03 {
 
     public static void main(String[] args) {
-        BaseBallTeam team01 = new BaseBallTeam();
-        team01.setBaseBallTeam("東京ヤクルトスワローズ", 80, 59, 4);
-        team01.getRate();
+        BaseBallTeam team01 = new BaseBallTeam("東京ヤクルトスワローズ", 80, 59, 4);
         team01.report();
         
-        BaseBallTeam team02 = new BaseBallTeam();
-        team02.setBaseBallTeam("横浜DeNAベイスターズ", 73, 68, 2);
-        team02.getRate();
+        BaseBallTeam team02 = new BaseBallTeam("横浜DeNAベイスターズ", 73, 68, 2);
         team02.report();
         
-        BaseBallTeam team03 = new BaseBallTeam();
-        team03.setBaseBallTeam("阪神タイガース", 68, 71, 2);
-        team03.getRate();
+        BaseBallTeam team03 = new BaseBallTeam("阪神タイガース", 68, 71, 2);
         team03.report();
         
-        BaseBallTeam team04 = new BaseBallTeam();
-        team04.setBaseBallTeam("読売ジャイアンツ", 68, 72, 3);
-        team04.getRate();
+        BaseBallTeam team04 = new BaseBallTeam("読売ジャイアンツ", 68, 72, 3);
         team04.report();
         
-        BaseBallTeam team05 = new BaseBallTeam();
-        team05.setBaseBallTeam("広島東洋カープ", 66, 74, 3);
-        team05.getRate();
+        BaseBallTeam team05 = new BaseBallTeam("広島東洋カープ", 66, 74, 3);
         team05.report();
         
-        BaseBallTeam team06 = new BaseBallTeam();
-        team06.setBaseBallTeam("中日ドラゴンズ", 66, 75, 2);
-        team06.getRate();
+        BaseBallTeam team06 = new BaseBallTeam("中日ドラゴンズ", 66, 75, 2);
         team06.report();
     }
 
